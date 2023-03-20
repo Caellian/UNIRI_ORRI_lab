36225943145a8b6c8debbf365b58348a32bf1c6d
FIDIT - studenti <korisnik@ODJ-O350-108.intra.uniri.hr>
2023-03-20 16:43:54 +0100

    Finish assignment
    Signed-off-by: FIDIT - studenti <korisnik@ODJ-O350-108.intra.uniri.hr>

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple() {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondBetweenAppleDrops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+            speed = System.Math.Abs(speed);
+        else if (pos.x > leftAndRightEdge)
+            speed = -System.Math.Abs(speed);
+    }
+
+    void FixedUpdate() {
+        if (Random.value < chanceToChangeDirections)
+            speed *= -1; //primjena smjera
+    }
+}
