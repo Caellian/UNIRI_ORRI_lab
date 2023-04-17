ffbdf40cb449edb56cb74f37cee9c80f835a13d5
FIDIT - studenti <korisnik@ODJ-O350-118.intra.uniri.hr>
2023-04-17 16:16:42 +0200

    Add bg parallax
    Signed-off-by: FIDIT - studenti <korisnik@ODJ-O350-118.intra.uniri.hr>

--- 1_movement/Assets/Scripts/CameraFollow.cs
+++ 1_movement/Assets/Scripts/CameraFollow.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraFollow : MonoBehaviour
 {
     private GameObject player;
     private Vector3 offset = new(0f, 1f, -1f);
 
+    private GameObject background;
+    private GameObject platforms;
+
     private void Awake()
     {
         player = GameObject.Find("Player");
+        background = GameObject.Find("background");
+        platforms = GameObject.Find("bgplatforms");
     }
 
     void LateUpdate()
     {
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0) + offset;
+        background.transform.position = new Vector3(transform.position.x, transform.position.y, background.transform.position.z);
+        platforms.transform.position = new Vector3(transform.position.x * 0.5f, transform.position.y * 0.5f, platforms.transform.position.z);
     }
 }
